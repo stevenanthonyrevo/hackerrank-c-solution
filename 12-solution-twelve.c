96622b19281801e241ebb5c883a01db61232246e
stevenanthonyrevo <stevenanthonyrevo@live.com>
2024-01-22 23:18:40 -0500

    update 12

--- 12-solution-twelve.c
+++ 12-solution-twelve.c
@@ -8,12 +8,13 @@ int main() {
     int sum = 0; 
     int n;    
     scanf("%d", &n); // n is the size of the array 
-    int *arr = (int*)malloc(n * sizeof(int)); // custom array   
-    for (char i = 0; i < n; i++) {
-        scanf("%d ", &arr[i]); // array contains space separated integer 
-        sum += arr[i];  // for each integer assigned to array assign new sum value    
+    arr=(int*)malloc(n * sizeof(int)); // custom array   
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]); // array contains space separated integer 
+    }
+    for (int i = 0; i < n; i++) {
+	sum += arr[i]; // for each integer assigned to array assign new sum value    
     }    
-    free(arr);  // free memory 
     printf("%d", sum); // print the total sum 
     return 0;
 }
